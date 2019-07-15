615630d8f27e6af8e1cefc883cf06e23fdeccc13
Ignacio Fontalvo <ifontalvo@MacBook-Pro.local>
2019-07-14 23:45:25 -0500

    adding filter function

--- index-lib.js
+++ index-lib.js
@@ -1,6 +1,12 @@
 const head = ([x, ...n]) => x;
 const tail = ([x, ...n]) => n;
 
+const filter = (arr, term) => {
+  return arr.filter(item => item.includes(term));
+};
+
 module.exports = {
-  head, tail
-}
+  head,
+  tail,
+  filter
+};
